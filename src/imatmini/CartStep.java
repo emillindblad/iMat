22359c9fb31cf65ac9b2ce3ffb4fb60243d0247d
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 18:38:11 +0200

    Label for total sum

--- src/imatmini/CartStep.java
+++ src/imatmini/CartStep.java
@@ -4,14 +4,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import se.chalmers.cse.dat216.project.ShoppingCart;
 
 import java.io.IOException;
 
 public class CartStep extends AnchorPane implements PurchaseSteps {
 
     @FXML private AnchorPane infoPane;
+
+    @FXML private Label sumLabel;
+
+    private final Model model = Model.getInstance();
+    private ShoppingCart shoppingCart;
 
     public CartStep() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("resources/views/cart_step.fxml"));
@@ -23,6 +30,13 @@ public class CartStep extends AnchorPane implements PurchaseSteps {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        updateCartStep();
+    }
+
+    public void  updateCartStep() {
+        this.shoppingCart = model.getShoppingCart();
+        sumLabel.setText("" + shoppingCart.getTotal() + " kr");
     }
 
     @Override

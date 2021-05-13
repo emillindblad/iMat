47ec2197ed4118c56d16964758b8d67ed701ae3c
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 18:15:12 +0200

    Added files for DeliveryStep

---
+++ src/imatmini/DeliveryStep.java
@@ -0,0 +1,15 @@
+package imatmini;
+
+import javafx.scene.layout.AnchorPane;
+
+public class DeliveryStep extends AnchorPane implements PurchaseSteps {
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void back() {
+
+    }
+}

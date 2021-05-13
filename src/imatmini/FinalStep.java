559fb40df49349672f97b0d7a8bc6637b0cb5e0b
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 18:15:41 +0200

    Added files for FinalStep

---
+++ src/imatmini/FinalStep.java
@@ -0,0 +1,15 @@
+package imatmini;
+
+import javafx.scene.layout.AnchorPane;
+
+public class FinalStep extends AnchorPane implements PurchaseSteps {
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void back() {
+
+    }
+}

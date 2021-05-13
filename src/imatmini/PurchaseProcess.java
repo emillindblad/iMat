1f9d1ab2b3ac943bb3e6c157af66c2886d9c14b1
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 18:38:31 +0200

    Wrote initialize method

--- src/imatmini/PurchaseProcess.java
+++ src/imatmini/PurchaseProcess.java
@@ -8,14 +8,27 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PurchaseProcess implements Initializable {
-    @FXML private AnchorPane cartStep;
-    @FXML private AnchorPane detailsStep;
-    @FXML private AnchorPane deliveryStep;
-    @FXML private AnchorPane finalStep;
+    @FXML private AnchorPane cartStepPane;
+    @FXML private AnchorPane detailsStepPane;
+    @FXML private AnchorPane deliveryStepPane;
+    @FXML private AnchorPane finalStepPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        DetailsStep detailsStep = new DetailsStep();
+        detailsStepPane.getChildren().add(detailsStep);
+        detailsStepPane.toBack();
 
+        DeliveryStep deliveryStep = new DeliveryStep();
+        deliveryStepPane.getChildren().add(deliveryStep);
+        deliveryStepPane.toBack();
+
+        FinalStep finalStep = new FinalStep();
+        finalStepPane.getChildren().add(finalStep);
+        finalStepPane.toBack();
+
+        CartStep cartStep = new CartStep();
+        cartStepPane.getChildren().add(cartStep);
     }
 
 

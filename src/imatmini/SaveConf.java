8d3d963f48df2fc64b982ceea1e8aa860880791a
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 14:03:36 +0200

    Added files for save conf.

---
+++ src/imatmini/SaveConf.java
@@ -0,0 +1,34 @@
+package imatmini;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class SaveConf extends AnchorPane {
+    private final MyInfo parentController;
+
+    @FXML private Button okButton;
+
+    public SaveConf(MyInfo parentController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("saveConf.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        this.parentController = parentController;
+    }
+
+    @FXML
+    public void onClose(Event event) {
+        parentController.onClose(event);
+    }
+}

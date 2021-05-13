676ea493dd12ee7112c98cd232d6e799949e5700
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-13 16:37:54 +0200

    Added title to application

--- src/imatmini/IMatMini.java
+++ src/imatmini/IMatMini.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -24,6 +25,7 @@ public class IMatMini extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setTitle("iMat - din onlinebutik");
         stage.show();
     }
 

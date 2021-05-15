cc2e4961629cff94342e3cb35958d01da7a9bec2
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-05-15 22:15:42 +0200

    Started working on final step

--- src/imatmini/FinalStep.java
+++ src/imatmini/FinalStep.java
@@ -3,15 +3,27 @@ package imatmini;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class FinalStep extends AnchorPane implements PurchaseStep {
 
     @FXML private AnchorPane infoPane;
-
+    @FXML private AnchorPane mainPane;
     @FXML private AnchorPane progressBar;
+
+    @FXML private AnchorPane purchaseCompletePane;
+    @FXML private Label endMessage;
+
+    @FXML private AnchorPane detailsPane;
+    @FXML private Label fullName;
+    @FXML private Label address;
+    @FXML private Label cityPostNum;
 
     private final PurchaseProcess parentProcess;
 
@@ -36,9 +48,36 @@ public class FinalStep extends AnchorPane implements PurchaseStep {
             progressBar.getChildren().add(new ProgressBar(this, 3));
     }
 
+
+
+    @FXML private void changeDetails(Event event) {
+        MyInfo myInfo = new MyInfo();
+        detailsPane.getChildren().add(myInfo);
+        myInfo.setLayoutX(60);
+        myInfo.setLayoutY(55);
+        detailsPane.toFront();
+    }
+
+    // ----------- Methods for completing purchase -----------
     @Override
     public void next() {
+        Model model = Model.getInstance();
+        model.placeOrder();
+        endMessage.setText("Tack " + model.getCustomer().getFirstName() + " för att du har handlat hos oss.");
+        purchaseCompletePane.toFront();
+    }
 
+    @FXML private void backToStart(Event event) {
+        try {
+            Stage stage = (Stage) infoPane.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("resources/views/iMatMini.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
@@ -46,6 +85,8 @@ public class FinalStep extends AnchorPane implements PurchaseStep {
         parentProcess.deliveryStep();
     }
 
+
+    // ----------- Methods for infoPane -----------
     @Override
     public void closeInfo() {
         infoPane.getChildren().clear();

c85b8026026486713cb876a0e7e076c771b628f1
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-05-17 12:16:59 +0200

    Added files for products for final step

---
+++ src/imatmini/resources/views/FinalStepProduct.java
@@ -0,0 +1,30 @@
+package imatmini.resources.views;
+
+import imatmini.Model;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingItem;
+
+public class FinalStepProduct extends AnchorPane {
+
+    @FXML private ImageView productImage;
+    @FXML private Label productName;
+    @FXML private Label productAmount;
+    @FXML private Label productSum;
+
+    public FinalStepProduct(ShoppingItem shoppingItem, boolean gray) {
+        Product product = shoppingItem.getProduct();
+
+        productImage.setImage(Model.getInstance().getImage(product, 150, 150));
+        productName.setText(product.getName());
+        productAmount.setText(shoppingItem.getAmount() + " st");
+        productSum.setText(shoppingItem.getTotal() + " kr");
+
+        if (gray)
+            this.setStyle("-fx-background-color: #D5D5D5");
+    }
+
+}

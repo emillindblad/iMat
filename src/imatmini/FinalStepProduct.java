705ba620b9e04890b108d5c01417359f90efce44
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-05-19 10:30:07 +0200

    Loaded fxml-file

--- src/imatmini/FinalStepProduct.java
+++ src/imatmini/FinalStepProduct.java
@@ -2,11 +2,14 @@ package imatmini;
 
 import imatmini.Model;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.Product;
 import se.chalmers.cse.dat216.project.ShoppingItem;
+
+import java.io.IOException;
 
 public class FinalStepProduct extends AnchorPane {
 
@@ -16,6 +19,16 @@ public class FinalStepProduct extends AnchorPane {
     @FXML private Label productSum;
 
     public FinalStepProduct(ShoppingItem shoppingItem, boolean gray) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("resources/views/final_step_product.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
         Product product = shoppingItem.getProduct();
 
         productImage.setImage(Model.getInstance().getImage(product, 150, 150));
